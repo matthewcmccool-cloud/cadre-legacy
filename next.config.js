dd3eb827455a089fe55da1acdd8ba60705a944e2
matthewcmccool-cloud <matthewcmccool@gmail.com>
2026-01-24 14:18:52 -0500

    fix: Simplify next.config.js to use SWC defaults

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify: false,
     transpilePackages: ['@clerk/nextjs', '@clerk/shared', '@clerk/backend'],
-    experimental: {
-        forceSwcTransforms: false,
-    },
 }
 
 module.exports = nextConfig
